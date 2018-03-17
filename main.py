50dc63b16968d3fa6a0bf392448e34bdd0a99919
lukeoftheaura <lukeoftheaura@protonmail.com>
2018-03-17 19:38:53 +0000

    WIP

---
+++ main.py
@@ -0,0 +1,23 @@
+import dataset
+import requests
+
+def download_game(g):
+    local_filename = g['Name'] + ".pkg"
+    # NOTE the stream=True parameter
+    r = requests.get(g['PKG direct link'], stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+    return local_filename
+
+db = dataset.connect("sqlite:///games.db")
+games = db['games']
+
+titleid = input("Title ID:")
+
+for g in games:
+    if g['Title ID'] == titleid:
+        print("Downloading, please wait")
+        download_game(g)
+        print("Download finished")
