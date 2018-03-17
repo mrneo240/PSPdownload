1aa7bbaed1962a3d0bca6f0af6ed50751e8495d3
lukeoftheaura <lukeoftheaura@protonmail.com>
2018-03-17 20:53:04 +0000

    added database auto-init function

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-import requests
+import requests, csv, sqlite3
 
 def download_game(g):
     local_filename = g['Name'] + ".pkg"
@@ -10,3 +10,17 @@ def download_game(g):
                 f.write(chunk)
     return local_filename
 
+def init_db():
+    con = sqlite3.connect("games.db")
+    cur = con.cursor()
+    cur.execute("CREATE TABLE games (\"Title ID\", \"Region\", \"Name\", \"PKG direct link\");")
+    
+    with open('PSP_GAMES.tsv','r') as fin:
+        dr = csv.DictReader(fin, delimiter="\t")
+        to_db = [(i['Title ID'], i['Region'], i['Name'],i['PKG direct link']) for i in dr]
+    
+    cur.executemany("INSERT INTO games (\"Title ID\", \"Region\", \"Name\", \"PKG direct link\") VALUES (?, ?, ?, ?);", to_db)
+    con.commit()
+    con.close()
+    
+    
